baea2013d095f718e8645b3e9246444fa4fcb6bf
dinesh <r.dinesh@directfn.com>
2018-12-09 21:40:09 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var ul = document.getElementById('todos');
+var add = document.getElementById('add');
+
+add.onclick = () => {
+    addToList(ul);
+};
+
+function addToList(targetUl) {
+    var inputText = document.getElementById('new-todo').value;
+    var li = document.createElement('li');
+    var textNode = document.createTextNode(inputText + ' ');
+    var done = document.createElement('input');
+    done.type = 'checkbox';
+    document.getElementById('new-todo').value = '';
+
+    if (inputText.split(' ').join('').length === 0) {
+        alert('Todos can not be empty');
+        return false;
+    }
+
+    done.className = 'completed';
+    done.innerHTML = '';
+    done.setAttribute('onclick', 'completed(this);');
+
+    li.appendChild(textNode);
+    li.appendChild(done);
+
+    targetUl.appendChild(li);
+}
+
+
+function completed(item) {
+    if(!item.parentNode.classList.contains('completed')){
+        item.parentNode.classList.add("completed");
+    }else{
+        item.parentNode.classList.remove("completed");
+    }
+
+}
